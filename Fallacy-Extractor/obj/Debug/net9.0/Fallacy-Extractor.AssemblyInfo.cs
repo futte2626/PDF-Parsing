579b1d11e605c86e7d62704da4b93db5bba67c7e
Sam-The-Man009 <117099086+Sam-The-Man009@users.noreply.github.com>
2026-01-30 13:55:31 +0100

    ):<

--- Fallacy-Extractor/obj/Debug/net9.0/Fallacy-Extractor.AssemblyInfo.cs
+++ Fallacy-Extractor/obj/Debug/net9.0/Fallacy-Extractor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fallacy-Extractor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c407d08b4e59461def0f3d370da40a97a99209b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58b5e6b8ee20b3de5e93dfc71dee1374afe21ace")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fallacy-Extractor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fallacy-Extractor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
